7230565ba850d0ca4109230d3b3d44e10f3e2c59
Paolo Salvatierra <psalvatierra@my.bcit.ca>
2022-10-25 11:31:24 -0700

    Add firebaseConfig info

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyAW42NGyBvzbvU4csTrSR3bRxVugZW8XtA",
+    authDomain: "comp1800-202230demo.firebaseapp.com",
+    projectId: "comp1800-202230demo",
+    storageBucket: "comp1800-202230demo.appspot.com",
+    messagingSenderId: "28681458570",
+    appId: "1:28681458570:web:358aaaf3f1dfd696072e73"
 };
 
 //--------------------------------------------
